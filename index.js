db64e94dd1876475d73b748ec22a3e38d6b6f15b
Entkenntnis <Entkenntnis@users.noreply.github.com>
2024-01-14 23:57:58 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+require('@entkenntnis/challenges-server')(config => {
+  // config.theme = 'yeti'
+  // config.locale = 'de
+  // config.brand = 'My-Server'
+  // config.slogan = 'Learning by challenges!'
+  /* config.i18nExtend.push({
+    lng: 'de',
+    key: 'contact.content_',
+    value: '<h1>Kontaktinformationen</h1>'
+  }) */
+  /* config.i18nExtend.push({
+    lng: 'de',
+    key: 'privacy.content_',
+    value: '<h1>Datenschutzerklärung</h1>'
+  }) */
+  return config
+})
